db8a158513723508746b8857655d2b723645a52e
IlakkiyanEK <ekilakkiyan@gmail.com>
2025-10-04 20:14:50 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,22 @@
+package mySQL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/restaurant_db";
+    private static final String USER = "root";  // your MySQL username
+    private static final String PASSWORD = "EKilakkiyan@25"; // your MySQL password
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
+
